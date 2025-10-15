aa47c5e648281c3179c43ea1c79f37adfe12ec5f
ahsan-f <140732909+ahsan-f@users.noreply.github.com>
2025-10-16 01:14:25 +0500

    Update and rename server.js to app.js

--- app/app.js
+++ app/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 app.get('/', (req, res) => {
-  res.send('Hello from the Minikube deployed Node.js app!');
+res.send('Hello from Kubernetes on Minikube! App is running.');
 });
-
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+console.log(App listening on port ${port}!);
 });
